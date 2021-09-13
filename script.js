0f785ba77ef8e60d641e19d7b2c5bd19c14e770a
Nathan <hui.n1998@gmail.com>
2021-09-13 22:21:15 +0100

    refactored styles to mobile first

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const navClose = document.getElementById("nav-close");
 const nav = document.getElementById("nav");
 const body = document.querySelector("body");
 
-console.log(body);
-
 function toggleMenu() {
     nav.classList.toggle("open");
     body.classList.toggle("stop-scrolling");
